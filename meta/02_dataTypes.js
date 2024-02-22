ea127aba8a2b963fcd4e338430bde9adb8f7cec3
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-02-22 15:19:52 +0000

    datatypes learned

---
+++ meta/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name="brocode"
+let age=34
+let isLoggedIn=true
+
+/*
+number
+bigint 
+string
+boolean
+null - standalone value
+undefined 
+symbol - unique 
+*/
+//object
+console.log(typeof "hello");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
+console.log(typeof undefined);
+console.log(typeof null);
