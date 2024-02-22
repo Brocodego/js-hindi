a17a86383fa29c48c5645c32a475763c2ef0b72c
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-02-22 15:19:32 +0000

    variables learned

---
+++ meta/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144552
+let accountEmail="rathourbro@gmail.com"
+var accountPassword="1234"
+accountCity="Gangoh"
+
+accountEmail="gangoh45@gmail.com"
+accountPassword="445566"
+accountCity="saharanput"
+let accountState;
+
+/*
+prefer nto to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
