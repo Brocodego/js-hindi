f2769cde454389a4d9843577f9b4ef225ad79d89
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-02-23 15:52:56 +0000

    operations learned

---
+++ meta/04_operations.js
@@ -0,0 +1,29 @@
+// let value=3
+// let negativeValue=-value
+// console.log(negativeValue);
+
+// let str1="hello"
+// let str2=" brocode"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+1+"2");
+// console.log("1"+1+2);
+
+// console.log(true);
+// console.log(+true);
+
+// let num1,num2,num3
+// num1=num2=num3 = 2+2 //this type of code writing is not good
+// console.log(num1);
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+let gameCounter2=100
+++gameCounter2
+console.log(gameCounter2);
+
